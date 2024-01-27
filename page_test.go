2b0d610eaa3f674a4cf959b8f5da28344e8c5297
kk <kk@kk>
2024-01-27 11:42:18 +0800

    rc

--- page_test.go
+++ page_test.go
@@ -7,6 +7,6 @@ func TestDatabase_Page(t *testing.T) {
 	fields, _ := build.BuildPage()
 	var expect = "LIMIT ? OFFSET ?"
 	assertsEqual(t, expect, fields)
-	expect = "SELECT * FROM `test_users` a GROUP BY `a`,`b` HAVING `id` IS NOT NULL ORDER BY id ASC LIMIT ? OFFSET ?"
+	expect = "SELECT * FROM `test_users` a GROUP BY `a`,`b` HAVING `id` IS NOT NULL ORDER BY `id` ASC LIMIT ? OFFSET ?"
 	assertsEqual(t, expect, build.ToSqlOnly())
 }
